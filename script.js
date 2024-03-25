261d6be86cf59445e7dc2c207b2ce8074f51039d
Esma Nur Kaşlıoğlu <esmakaslioglu@gmail.com>
2024-03-26 02:13:36 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+let currentIndex = 1;
+const maxIndex = 3; // Toplam resim sayısı
+
+function changeImage(step) {
+    currentIndex += step;
+    if (currentIndex < 1) {
+        currentIndex = maxIndex;
+    } else if (currentIndex > maxIndex) {
+        currentIndex = 1;
+    }
+    document.getElementById('image').src = 'image' + currentIndex + '.jpg';
+}
+function scrollCards(direction) {
+    const container = document.querySelector('.cards');
+    const cardWidth = document.querySelector('.firma-card').offsetWidth + 10; // Width of card plus margin
+    const scrollAmount = direction * (cardWidth * 2); // Scroll two cards at a time
+
+    container.scrollLeft += scrollAmount;
+}
